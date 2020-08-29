2bf262a844485a1996d8322ee3392b5e56a71543
cn92 <54868389+cn92@users.noreply.github.com>
2020-08-29 13:09:38 -0400

    Create nav.js

---
+++ nav.js
@@ -0,0 +1,16 @@
+class Navigation extends React.Component {
+
+    render() {
+      return(
+
+      <h1>Hello World!</h1>
+
+      );
+    }
+}
+  
+
+  ReactDOM.render( 
+    <Navigation />, 
+    document.getElementById('navcontain') 
+  );
