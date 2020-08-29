f61f65ff4f95daa82b67743920c8371db018004f
cn92 <54868389+cn92@users.noreply.github.com>
2020-08-29 13:03:36 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -1,8 +1,16 @@
 class Nav extends React.Component {
+
+    constructor(props) {
+        super(props);
+        };
+    
+
     render() {
-      return <h1>Hello World!</h1>
+      return 
+      <h1>Hello World!</h1>;
     }
-  }
+}
+  
 
   ReactDOM.render(<Nav />, 
     document.getElementById('nav_container')
