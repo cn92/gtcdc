1e84d84d610924396d9a6e0ecc9738bd996a62ba
cn92 <54868389+cn92@users.noreply.github.com>
2020-08-29 12:49:39 -0400

    Update nav.js

--- nav.js
+++ nav.js
@@ -3,38 +3,21 @@
 const e = React.createElement;
 
 class Nav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
 
   render() {
-    <nav class="tnav">
-    <div class="logo">The Giving Tree</div>
-    <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">About Us</a>
-            <ul id="submenu">
-                <li><a href="#">Mission & Philosophy</a></li>
-                <li><a href="#">Our Approach</a></li>
-                <li><a href="#">General Information</a></li>
-            </ul>
-        </li>
-        <li><a href="#">Programs</a>
-            <ul id="submenu">
-                <li><a href="#">Baby Bears</a></li>
-                <li><a href="#">Lil' Bears</a></li>
-                <li><a href="#">Honey Bears</a></li>
-                <li><a href="#">Chubby Cheetahs</a></li>
-                <li><a href="#">Lil' Tigers</a></li>
-                <li><a href="#">Lil' Lions</a></li>
-                <li><a href="#">Lil' Zebras</a></li>
-                <hr />
-                <li><a href="#">Pre-K</a></li>
-                <li><a href="#">Before & After School</a></li>
-            </ul>
-        </li>
+    if (this.state.liked) {
+      return 'You liked this.';
+    }
 
-        <li><a href="#">Extracurriculars</a></li>
-        <li><a href="#">Contact</a></li>
-    </ul>
-</nav>
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
   }
 }
 
